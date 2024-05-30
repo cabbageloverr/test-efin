d25ef21b92c97b3dc5dc4ddcf8247819cf227840
cabbageloverr <natthimanoinon@gmail.com>
2024-05-30 11:42:50 +0700

    fix text

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,9 @@
     <div class="body h-100 p-2 grid grid-cols-4 grid-rows-4 gap-4 text-white text-center">
       <div class="bg-red-800 row-span-2 col-span-2">{{ data?.data_1 }} </div>
       <div class="bg-green-800 row-span-2 col-span-1">{{ data?.data_2 }}</div>
-      <div class="bg-purple-800 row-span-4 col-span-1">{{ data?.data_3 }}</div>
-      <div class="bg-indigo-800 col-span-2">{{ data?.data_4 }}</div>
-      <div class="bg-teal-800 col-span-1">{{ data?.data_5 }}</div>
+      <div class="bg-purple-800 row-span-4 col-span-1">{{ data?.data_5 }}</div>
+      <div class="bg-indigo-800 col-span-2">{{ data?.data_3 }}</div>
+      <div class="bg-teal-800 col-span-1">{{ data?.data_4 }}</div>
       <div class="bg-yellow-800 row-span-1 col-span-3 ">{{ data?.data_6 }}</div>
     </div>
   </div>
